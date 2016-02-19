7f9082c3d1d8fdae1586b83b3b30939480e418f2
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-02-19 13:00:17 +0100

    workaround

--- client/main.js
+++ client/main.js
@@ -3,5 +3,12 @@ import '1237anjasdbhsbd712b3hbas'; // it's a temp npm package of angular-meteor
 import './app.html';
 
 angular.module('app', [
-  'angular-meteor'
-]);
+    'angular-meteor',
+    'angular-templates'
+  ])
+  .directive('app', () => ({
+    templateUrl: 'client/app.html',
+    controller($scope) {
+      $scope.test = 'it works!';
+    }
+  }))
